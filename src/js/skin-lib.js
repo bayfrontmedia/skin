443fe006b20d64295be213250039133ceda8321a
John Robinson <john@bayfrontmedia.com>
2025-03-28 12:47:12 -0400

    Miscellaneous updates

--- src/js/skin-lib.js
+++ src/js/skin-lib.js
@@ -4,16 +4,12 @@ import * as Helpers from "./modules/helpers";
 import * as Http from "./modules/http";
 import * as Theme from "./modules/theme";
 
-exports.init = function() {
+Theme.detect();
 
-    Theme.detect();
-
-    window.Skin = {
-        Avow,
-        Console,
-        Helpers,
-        Http,
-        Theme
-    }
-
+window.Skin = {
+    Avow,
+    Console,
+    Helpers,
+    Http,
+    Theme
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
-const appJs = {
+const skinJs = {
     entry: path.resolve(__dirname, 'src/js/skin.js'),
     output: {
-        path: path.resolve(__dirname, 'docs/assets/js'),
+        path: path.resolve(__dirname, 'dist/js'),
         filename: 'skin.js'
     }
 }
 
-module.exports = [appJs];
+const skinLibJs = {
+    entry: path.resolve(__dirname, 'src/js/skin-lib.js'),
+    output: {
+        path: path.resolve(__dirname, 'docs/assets/js'),
+        filename: 'skin-lib.js'
+    }
+}
+
+module.exports = [skinJs, skinLibJs];
