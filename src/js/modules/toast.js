c703d8841845c8b35235711e7177f0668e16646c
John Robinson <john@bayfrontmedia.com>
2025-03-30 09:23:14 -0400

    Updated toast

--- src/js/modules/toast.js
+++ src/js/modules/toast.js
@@ -1,5 +1,9 @@
 import Toastify from 'toastify-js';
 
+/*
+See: https://github.com/apvarun/toastify-js/tree/master
+ */
+
 /**
  * Queue a toast to be shown with showQueue method by saving to localStorage.
  *
@@ -18,7 +22,7 @@ export function queue(config = {}, lang = {}) {
 
     }
 
-    toasts.push({ config: config, lang: lang });
+    toasts.push({config: config, lang: lang});
     localStorage.toasts = JSON.stringify(toasts);
 
 }
@@ -83,7 +87,10 @@ export function show(config = {}, lang = {}) {
 
     lang = {...defaultLang, ...lang};
 
-    config.text = '<div class="text-sm">' + config.text + '</div><button onclick="Skin.Toast.close(this)" type="button" class="ms-auto size-6 ac-btn-close" aria-label="' + lang.close + '"><span class="sr-only">' + lang.close + '</span><span class="iconify heroicons--x-mark-solid text-base"></span></button>';
+    if (config.close === true) {
+        config.text += '<button onclick="Skin.Toast.close(this)" type="button" class="tc-toast-close" aria-label="' + lang.close + '"><span class="sr-only">' + lang.close + '</span><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" /></svg></span></button>';
+    }
+
     config.className = config.className + " tc-toast";
 
     Toastify(config).showToast();
